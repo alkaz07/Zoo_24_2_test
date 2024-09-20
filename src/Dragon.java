645fa49859ca1718bae290089c1fb9ba0712cf3d
Tony-StPet <dreamhacker51@icloud.com>
2024-09-20 14:47:23 +0300

    Дракон vol.2

---
+++ src/Dragon.java
@@ -0,0 +1,26 @@
+public class Dragon {
+
+        String majesticName;
+        int age;
+        String color;
+        boolean transform;
+        String element;
+        public Dragon(String majesticName, int age, String color, boolean transform,String element) {
+            this.majesticName = majesticName;
+            this.age = age;
+            this.color = color;
+            this.transform = transform;
+            this.element = element;
+        }
+        public void drakaris(){
+            System.out.println(" make Fire ");
+        }
+
+        public void move(){
+        System.out.println(" Dragon fly to the sky ");}
+
+        public void makeSound(){
+        System.out.println(" оглушительный рык");}
+
+
+}
