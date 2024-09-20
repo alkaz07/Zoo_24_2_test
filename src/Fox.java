6a72e8cb47628c11f0bf3606e04fce1575e61114
dashkov.v.e <dmitrieva.kg@gmail.com>
2024-09-20 15:43:55 +0300

    Вот лиса.

---
+++ src/Fox.java
@@ -0,0 +1,18 @@
+public class Fox {
+    String name,color;
+    int age;
+    int amountchildren;
+    public  void makeSound(){
+        System.out.println( "Tyav,tyav");
+        }
+    public static void main(String[] args) {
+     Fox cunning = new Fox("Белая",5,0,);
+    }
+
+  public Fox(String color,int age,int amountchildren){
+        this.color = color;
+        this.age = age;
+        this.amountchildren = amountchildren;
+
+  }
+}
