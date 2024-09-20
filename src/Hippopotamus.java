70a4cd3db6714bf97107ed2b569475ba6d4bb45c
MarockoB <Marocko.bs@mail.ru>
2024-09-20 12:45:55 +0300

    Add files via upload

---
+++ src/Hippopotamus.java
@@ -0,0 +1,11 @@
+class Hippopotamus{
+    public static String color(){
+        return "Серый";
+    }
+    public static String name(){
+        return "Бочка";
+    }
+    public static String age(){
+        return "7 лет";
+    }
+}
