13456e8e5e5d3b84476c521c72d85b8b93b55cc9
alkaz07 <alkaz07@yandex.ru>
2024-09-20 16:07:29 +0300

    Переходим на 23 JDK

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Elephant hathi = new Elephant("Вася", 600, 2 );
         hathi.makeSound();
 
-        Hippopotamus hippopotamus = new Hippopotamus();
+       // Hippopotamus hippopotamus = new Hippopotamus();
         Dog dingo = new Dog("черный", "Петербург", 321, "");
         dingo.bark();
         Dragon dragon =new Dragon("демигоргон", 1568, "красный", true, "frost");
