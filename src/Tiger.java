5f61794a9b6477dea9726f1031a22c14a151bff0
Tumanov <tslavich@bk.ru>
2024-09-20 14:24:46 +0300

    Tiger -Add tiger

---
+++ src/Tiger.java
@@ -0,0 +1,14 @@
+public class Tiger {
+    int age;
+    String name, breed;
+
+    public void voice() {
+        System.out.println("R-r-r-r-r-r");
+    }
+
+    public Tiger(int age, String name, String breed) {
+        this.age = age;
+        this.name = name;
+        this.breed = breed;
+    }
+}
