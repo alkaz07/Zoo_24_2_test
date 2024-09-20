2929942232d62680ec87bf4bc31cdebd96298635
balteyes <julian.ivanova@gmail.com>
2024-09-20 15:54:21 +0300

    добавлены действия у Cat

--- src/Cat.java
+++ src/Cat.java
@@ -1,22 +1,22 @@
 class Cat {
     String color, name, breed;
     byte age, weight;
-    public void sleep (){
-        System.out.println("sleep");
-    }
 
-
-    public String ability() {
-        return "Мяу";
-
-    }
-
-    public Cat(String color, String name, String breed, byte age, byte weight, String ability) {
+    public Cat(String color, String name, String breed, byte age, byte weight) {
         this.color = color;
         this.name = name;
         this.breed = breed;
         this.age = age;
         this.weight = weight;
+    }
+
+    public void sleep (){
+        System.out.println("sleep");
+    }
+
+    public String ability() {
+        return "Мяу";
+
     }
 
     public void askFood (){
