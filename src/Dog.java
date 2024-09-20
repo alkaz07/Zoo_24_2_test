2f3ae368492796dd2cba2b8315cf282057b77e56
I12ina <5000hardworkers@gmail.com>
2024-09-20 14:29:27 +0300

    Second step

--- src/Dog.java
+++ src/Dog.java
@@ -1,26 +1,28 @@
-public static void main(String[] args) {
+public class Dog {
+    public static void main(String[] args) {
 
-    Dog dog = new Dog("Белая", "Берта", 2, "Кавказская овчарка");
-    dog.bark();
-}
-
-// Класс Собачка
-static class Dog {
-    String color;
-    String name;
-    int age;
-    String breed;
-
-    // Конструктор
-    public Dog(String color, String name, int age, String breed) {
-        this.color = color;
-        this.name = name;
-        this.age = age;
-        this.breed = breed;
+        Dog dog = new Dog("Белая", "Берта", 2, "Кавказская овчарка");
+        dog.bark();
     }
 
-    // Метод гавкания
-    public void bark() {
-        System.out.println(name + " говорит: Гав!");
+    // Класс Собачка
+    static class Dog {
+        String color;
+        String name;
+        int age;
+        String breed;
+
+        // Конструктор
+        public Dog(String color, String name, int age, String breed) {
+            this.color = color;
+            this.name = name;
+            this.age = age;
+            this.breed = breed;
+        }
+
+        // Метод гавкания
+        public void bark() {
+            System.out.println(name + " говорит: Гав!");
+        }
     }
 }
