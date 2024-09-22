848c696fb87b983091df62c4f449298d58b2becd
Harry Tothmure <harrytot@student.21-school.ru>
2024-09-22 20:11:45 +0300

    +Actions

--- src/Dog.java
+++ src/Dog.java
@@ -5,6 +5,7 @@ public class Dog {
         String name;
         int age;
         String breed;
+        int weight;
 
         // Конструктор
         public Dog(String color, String name, int age, String breed) {
@@ -12,10 +13,11 @@ public class Dog {
             this.name = name;
             this.age = age;
             this.breed = breed;
+            this.weight = weight;
         }
 
         // Метод гавкания
-        public void bark() {System.out.println(name + " говорит: Гав!");}
+        public void bark() {System.out.println(name + " говорит: Тихо, я сказал! Или я тихо сказал?");}
         //Метод бегания
         public void run() {System.out.println(name + " живо кружится вокруг доброго человека и приветствует, радостно виляя хвостиком");}
 
