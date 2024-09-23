470c09043abedfb2e1ac9b9ca610a81467b455e7
SerzhSeryozhka <Jk3103KS@yandex.ru>
2024-09-23 10:30:03 +0300

    ДОбавлен Волк, Енот

---
+++ src/Wolf.java
@@ -0,0 +1,14 @@
+public class Wolf {
+    String color; // цвет
+    int weight; //вес
+    int height; // высота
+
+    public Wolf (String color, int weight, int height) {
+        this.color = color;
+        this.weight = weight;
+        this.height = height;
+
+
+
+    }
+}
