8ae6d013d4a3cfe9ec32af26701c786be0aa6621
Tony-StPet <135145428+Tony-StPet@users.noreply.github.com>
2024-09-23 08:33:13 +0300

    Platypus, Narval add by Tony K

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,10 @@ public class Main {
         dragon.move();
         Bear medved   = new Bear ("Цвет", 99, 33);
 
+        Narval narval = new Narval("Вилли", 39, 800,  2, "сине-серый");
+        narval.makeSound(); narval.makeSound2(); narval.move(); narval.move2();
+
+        Platypus platypus = new Platypus("Агент Пи", 2, 1,30, "brown");
+        platypus.move(); platypus.move2(); platypus.makeSound();
     }
 }
