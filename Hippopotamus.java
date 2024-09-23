1a6b2721dfe517843c8b489e408b44eda4d991c3
MarockoB <Marocko.bs@mail.ru>
2024-09-23 09:54:13 +0300

    update grammar

---
+++ Hippopotamus.java
@@ -0,0 +1,24 @@
+public class Hippopotamus {
+    String name;
+    String color;
+    int weight;
+    int age;
+
+    public Hippopotamus(String name, String color, int weight, int age) {
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public void topTop(){
+        System.out.println("Run away to swim");
+    }
+
+    public void swimSwim(){System.out.println("Under the water");
+    }
+
+    public void watermelon(){
+        System.out.println("Destroys watermelons");
+    }
+}
