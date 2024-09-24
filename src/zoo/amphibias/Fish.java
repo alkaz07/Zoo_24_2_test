e99758249e542c3988dadea5cd1673977f94d1ab
I12ina <5000hardworkers@gmail.com>
2024-09-24 13:12:04 +0300

    +zoo.animal.amphibias.Fish

--- src/zoo/amphibias/Fish.java
+++ src/zoo/amphibias/Fish.java
@@ -2,7 +2,7 @@ package zoo.amphibias;
 
 import zoo.Animal;
 
-abstract public class Fish extends Animal {
+abstract public class Fish extends Amphibia {
         protected double typeOfWater;
 
         public Fish(double typeOfWater) {this.typeOfWater = typeOfWater;}
